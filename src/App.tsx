f83a8433f681f7d4c3af2d7ec962420c3dabc684
wisnievskikarol <wisnievskikarol@gmail.com>
2022-07-11 13:56:43 +0200

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+// import "dotenv/config";
 
 function App() {
+  const { REACT_APP_TEST_KEY } = process.env;
+
+  console.log("Kurwa", REACT_APP_TEST_KEY);
+  console.log("Kurwa2", process.env.REACT_APP_TEST_KEY);
   return (
     <div className="App">
       <header className="App-header">
